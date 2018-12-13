5de5e44ba34501783ae407d7513fc38da95e5fe5
uzusun <ektkfdl@naver.com>
2018-12-13 17:16:22 +0900

    solved

--- python/example_114_4.py
+++ python/example_114_4.py
@@ -8,8 +8,21 @@ sys.stderr = io.TextIOWrapper(sys.stderr.detach(), encoding = 'utf-8')
 
 num_list = list()
 for a in range(3):
-    a =+ 1
+    a += 1
     num = input("%d 번째 숫자를 입력하세요 : " % a)
     num_list.append(num)
+
+if num_list[0] > num_list[1]:
+    temp = num_list[0]
+    num_list[0] = num_list[1]
+    num_list[1] = temp
+    if num_list[1] > num_list[2]:
+        temp = num_list[1]
+        num_list[1] = num_list[2]
+        num_list[2] = temp
+        if num_list[0] > num_list[1]:
+            temp = num_list[0]
+            num_list[0] = num_list[1]
+            num_list[1] = temp
 
 print(num_list)
