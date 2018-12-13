0796d0a04d9db3b87e64ff095b00f3dbb81b9158
uzusun <ektkfdl@naver.com>
2018-12-13 14:18:57 +0900

    예제 추가

--- python/example_114_4.py
+++ python/example_114_4.py
@@ -5,3 +5,11 @@ import io
 
 sys.stdout = io.TextIOWrapper(sys.stdout.detach(), encoding = 'utf-8')
 sys.stderr = io.TextIOWrapper(sys.stderr.detach(), encoding = 'utf-8')
+
+num_list = list()
+for a in range(3):
+    a =+ 1
+    num = input("%d 번째 숫자를 입력하세요 : " % a)
+    num_list.append(num)
+
+print(num_list)
