d588acda198a0c7da42f31feca111abd69952f8c
uzusun <ektkfdl@naver.com>
2018-12-17 18:05:28 +0900

    delete ansible folder

--- ansible/add_ssh_auth.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-#ssh key 생성
-sshpass -p vagrant ssh -T -o StrictHostKeyChecking=no vagrant@192.168.0.16
-sshpass -p vagrant ssh -T -o StrictHostKeyChecking=no vagrant@192.168.0.17
-sshpass -p vagrant ssh -T -o StrictHostKeyChecking=no vagrant@192.168.0.18
